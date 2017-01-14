1aabf4a56cf39b8c64a01a536458a68b46a31954
Haochi Chen <github@ihaochi.com>
2017-01-14 14:41:25 -0300

    Init commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Install
+
+```html
+<script src="angular-mouse-tooltip.js></script>
+```
+
+```javascript
+angular.module('myApp', ['ihaochi'])
+```
+
+# Usage
+
+```html
+<ihaochi-mouse-tooltip ng-show="visible">hello world</ihaochi-mouse-tooltip>
+```
+
+# Scopes
+
+* `x-offset: '='`: default = `10`
+* `y-offset: '='`: default = `10`
---
+++ src/angular-mouse-tooltip.js
@@ -0,0 +1,32 @@
+angular.module('ihaochi', [])
+.directive("ihaochiMouseTooltip", ['$window', '$document', function ($window, $document) {
+    var defaultOffset = 10;
+
+    return {
+        scope: {
+            xOffset: '=',
+            yOffset: '='
+        },
+        transclude: true,
+        template: '<div ng-transclude></div>',
+        link: function(scope, element) {
+            var mouseMove = "mousemove";
+
+            var mouseMoveListener = function(e) {
+                var xOffset = Number.isFinite(scope.xOffset) ? scope.xOffset : defaultOffset;
+                var yOffset = Number.isFinite(scope.yOffset) ? scope.yOffset : defaultOffset;
+
+                element.css("left", (e.clientX + xOffset) + 'px');
+                element.css("top", (e.clientY + yOffset) + 'px');
+            };
+
+            $document.on(mouseMove, mouseMoveListener);
+
+            element.css("position", "absolute");
+            element.on('$destroy', function() {
+                $document.off(mouseMove, mouseMoveListener);
+            });
+	    
+        }
+    };
+}]);
